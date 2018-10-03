30539eb441e99660e61d3940e58aaa67d0a83aa7
lieoncx <lieoncx@gmail.com>
2018-10-03 23:15:04 +0800

    update

---
+++ Flask/App.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, Response, redirect, flash, url_for, request
+from flask_
+from flask_sqlalchemy import SQLAlchemy
+import json
+from flask_bcrypt import Bcrypt
+from flask_login import current_user, login_user, logout_user, login_required
+from Forms import RegistrationForm, LoginForm
+from Models import User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def home():
+    jsonstr =  {
+        "ewr": "asdf",
+        "adsf": 'afdadsf'
+    }
+    return  json.dumps(jsonstr)
+
+@app.route('/about')
+def about():
+    render_template('about.html', title="About")
+
+@app.route('/register', methods=["GET", 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_passwor = bcrypt.generate_password_hash(form.password.data).decode('urf-8')
+        user = User(username=form.username.data, email = form.email.data, password=hashed_passwor)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for='login')
+    return render_template('register.html', title="register", form=form)
+
+
+@app.route('/logoin', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('home')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.fliter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('lognin.html', title="login", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
