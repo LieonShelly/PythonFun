5d4950d156dc944e797911cb53e0373fdb1f4fdb
lieoncx <lieoncx@gmail.com>
2018-10-13 00:38:42 +0800

    mysql

--- Flask/Blog/config.py
+++ Flask/Blog/config.py
@@ -1,8 +1,19 @@
 import os
 
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD') 
+DATABASE = 'blog'
+USER = 'root'
+
 class Config:
         SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-        SQLALCHEMY_DATABASE_URI = "mysql://pythonfun-219016:us-central1:pythonmysql:lieon1992316@130.211.127.141/blog"
+        SQLALCHEMY_DATABASE_URI = (
+        'mysql+pymysql://{user}:{password}@localhost/{database}' 
+        '?unix_socket=/cloudsql/{connection_name}').format(
+        user=USER, password=CLOUDSQL_PASSWORD, 
+        database=DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME) 
+
         MAIL_SERVER = 'smtp.googlemail.com'
         MAIL_PORT = 587
         MAIL_USE_TLS = True
