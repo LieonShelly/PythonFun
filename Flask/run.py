0ebf68aef8103ee283e6bd207113aedfbcd35e8c
lieoncx <lieoncx@gmail.com>
2018-10-13 01:46:47 +0800

    fix

--- Flask/run.py
+++ Flask/run.py
@@ -4,6 +4,5 @@ import requests
 app = create_app()
 
 if __name__ == '__main__':
-    db.create_all(app=app)
     app.run(host='127.0.0.1', port=8080, debug=True)
     # app.run(host='0.0.0.0', port=80)
