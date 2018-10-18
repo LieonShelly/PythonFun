e1956b6fbe90fff16053e086941fb3fa7756e9e6
lieoncx <lieoncx@gmail.com>
2018-10-18 17:02:31 +0800

    update

--- Flask/Blog/config.py
+++ Flask/Blog/config.py
@@ -6,14 +6,15 @@ CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 DATABASE = 'blog'
 USER = 'root'
 
+DB4_PORT_3306_TCP_ADDR = os.environ.get('DB4_PORT_3306_TCP_ADDR') 
+DB4_PORT_3306_TCP_PORT = os.environ.get('DB4_PORT_3306_TCP_PORT') 
+DB4_ENV_MYSQL_ROOT_PASSWORD = os.environ.get('DB4_ENV_MYSQL_ROOT_PASSWORD') 
+# mysql+pymysql://root:password@localhost:3306/test
+MysqlURI = "mysql://root:{DB4_ENV_MYSQL_ROOT_PASSWORD}@{DB4_PORT_3306_TCP_ADDR}:{DB4_PORT_3306_TCP_PORT}/blog".format(DB4_ENV_MYSQL_ROOT_PASSWORD=DB4_ENV_MYSQL_ROOT_PASSWORD,DB4_PORT_3306_TCP_ADDR=DB4_PORT_3306_TCP_ADDR,DB4_PORT_3306_TCP_PORT=DB4_PORT_3306_TCP_PORT)
 class Config:
-        SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-        SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://{user}:{password}@localhost/{database}' 
-        '?unix_socket=/cloudsql/{connection_name}').format(
-        user=USER, password=CLOUDSQL_PASSWORD, 
-        database=DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME) 
 
+        SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+        SQLALCHEMY_DATABASE_URI = MysqlURI
         MAIL_SERVER = 'smtp.googlemail.com'
         MAIL_PORT = 587
         MAIL_USE_TLS = True
