5d03ebbc483eed178b71b0a81f97a284dd97ae56
lieoncx <lieoncx@gmail.com>
2018-10-25 15:36:40 +0800

    gunicorn

---
+++ Flask/gunicorn.py
@@ -0,0 +1,6 @@
+import Blog
+
+bind = "0.0.0.0:5000" 
+# accesslog = '/var/log/gunicorn/gunicorn-access.log'
+# errorlog = '/var/log/gunicorn/gunicorn-error.log'
+# loglevel = 'info'
--- Flask/run.py
+++ Flask/run.py
@@ -5,4 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=8080, debug=True)
+    print('=====runing=========')
     app.run(host='0.0.0.0', port=80)
