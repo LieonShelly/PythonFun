091bce168544a4606ce4d529d38f52d5fe8fd623
lieoncx <lieoncx@gmail.com>
2018-10-27 01:12:00 +0800

    q2q2

--- Flask/Blog/config.py
+++ Flask/Blog/config.py
@@ -1,16 +1,6 @@
 import os
 
-CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
-CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
-CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD') 
-DATABASE = 'blog'
-USER = 'root'
-
-DB4_PORT_3306_TCP_ADDR = os.environ.get('DB4_PORT_3306_TCP_ADDR') 
-DB4_PORT_3306_TCP_PORT = os.environ.get('DB4_PORT_3306_TCP_PORT') 
-DB4_ENV_MYSQL_ROOT_PASSWORD = os.environ.get('DB4_ENV_MYSQL_ROOT_PASSWORD') 
-# mysql+pymysql://root:password@localhost:3306/test
-MysqlURI = "mysql://root:{DB4_ENV_MYSQL_ROOT_PASSWORD}@{DB4_PORT_3306_TCP_ADDR}:{DB4_PORT_3306_TCP_PORT}/blog".format(DB4_ENV_MYSQL_ROOT_PASSWORD=DB4_ENV_MYSQL_ROOT_PASSWORD,DB4_PORT_3306_TCP_ADDR=DB4_PORT_3306_TCP_ADDR,DB4_PORT_3306_TCP_PORT=DB4_PORT_3306_TCP_PORT)
+MysqlURI = "mysql://root:123456@mysql:3306/blog" # mysql yml文中的容器名称
 print('========MysqlURI=====', MysqlURI)
 class Config:
 
