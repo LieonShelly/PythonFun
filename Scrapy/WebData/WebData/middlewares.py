13fc40b94344b7190e46db391485d16ebd10962a
lieoncx <lieoncx@gmail.com>
2018-11-05 16:08:25 +0800

    爬虫获取代理IP

--- Scrapy/WebData/WebData/middlewares.py
+++ Scrapy/WebData/WebData/middlewares.py
@@ -6,7 +6,8 @@
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
-
+import requests
+from WebData.settings import *
 
 class WebdataSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
@@ -101,3 +102,57 @@ class WebdataDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class ProxyMiddware(object):
+
+    def __init__(self, proxyUrl):
+        self.proxyUrl = proxyUrl
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            proxyUrl = ProxyURL
+        )
+
+    def process_request(self, request, spider):
+        if request.meta.get('retry_times'):
+            proxy = self.getRandomProxy()
+            if proxy:
+                uri = "http://" + str(proxy)
+                request.meta["proxy"] = uri
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+    def getRandomProxy(self):
+        try:
+           response = requests.get(url=self.proxyUrl)
+           if response.status_code == 200:
+               proxy = response.text
+               return proxy
+        except requests.ConnectionError:
+            return False
+
+
+
+
