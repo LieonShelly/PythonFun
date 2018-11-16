ea2fab3ffb1683988bb9f08d006e7f35f921b34c
lieoncx <lieoncx@gmail.com>
2018-11-16 11:25:18 +0800

    update

--- Flasky/app/auth/models.py
+++ Flasky/app/auth/models.py
@@ -1,5 +1,11 @@
 
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from flask_login import UserMixin, AnonymousUserMixin
+from app import db, loginManager
+
 
 class Permission:
     FOLLOW = 1
@@ -25,6 +31,33 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.email
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @property.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verfiy_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+    
+    def confirm(self, token):
+        s = Serializer(current_app.config('SECRET_KEY'))
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') ! = self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
   
 class Role(db.Model):
     __tablename__='roles'
@@ -32,7 +65,7 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     defalt = db.Column(db.Boolean, defalt=False, index=True)
     permissions = db.Column(db.Integer)
-     users = db.relationship('User', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
          return '<Role %r>' % self.name
